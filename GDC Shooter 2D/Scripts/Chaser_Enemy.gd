72b1c08329a28babb8b37bb3b7a497918d897933
Archit-Dubey <architdubey2003@gmail.com>
2023-11-15 11:59:08 +0530

    Added Chaser_Enemy into the game, Spawner has not been added yet

---
+++ GDC Shooter 2D/Scripts/Chaser_Enemy.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+
+@export var speed = 200  # Adjust this value to control the speed of the enemy
+@onready var player=$"../Player"
+
+func _ready():
+		# Start moving towards the player
+	if player:
+		print("Yes")
+	else:
+		print("No")
+
+func _process(delta):
+	if player:
+		# Calculate the direction to the player
+		var direction = (player.global_position - global_position).normalized()
+
+		# Move the enemy towards the player
+		velocity = direction * speed
+		
+		# Make the enemy look at player all the time
+		look_at(player.global_position)
+
+		move_and_slide()
+		
+	else:
+		print()
+
+
