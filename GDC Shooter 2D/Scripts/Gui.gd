fb7624cb54af2ab48ab599b24c352b792508ead2
Pranav <pranavhp871@gmail.com>
2023-12-01 16:52:21 +0530

    Added a pause screen

--- GDC Shooter 2D/Scripts/Gui.gd
+++ GDC Shooter 2D/Scripts/Gui.gd
@@ -7,11 +7,14 @@ var save_path = "user://userdata.save"
 @onready var player=$"../Player"
 @onready var scoreboard=$Score
 @onready var highscore=$HighScore
+@onready var pauseAnim=$PauseScreen/PauseAnim
 
 var currScore=0
 var currHighscore=0
 
 func _ready():
+	
+	
 	incScore(0)
 	health.max_value=player.maxHealth #set the max to the players max health
 	lives.text=str(player.lives)
@@ -41,12 +44,40 @@ func updatePlayerHealth(num): #updates gui bar (does not affect player, this is 
 func incScore(num):
 	currScore+=num
 	scoreboard.text="Score: "+str(currScore)
-	if currScore > currHighscore:
-		currHighscore = currScore
-		save_high_score()
-		highscore.text = "High Score: " + str(currHighscore)
+	
+	save_high_score() #moved checking logic to function
+		
 	
 # Save high score to local storage
 func save_high_score():
-	var file = FileAccess.open(save_path, FileAccess.WRITE)
-	file.store_var(currHighscore)
+	
+	if currScore > currHighscore: 
+		currHighscore = currScore
+		var file = FileAccess.open(save_path, FileAccess.WRITE)
+		file.store_var(currHighscore)
+		
+		highscore.text = "High Score: " + str(currHighscore)
+
+func _on_pause_pressed():
+	if get_tree().paused:
+		get_tree().paused=false
+		pauseAnim.play_backwards("Pause")
+	else:
+		get_tree().paused=true
+		pauseAnim.play("Pause")
+
+
+func _on_resume_pressed():
+	get_tree().paused=false
+	pauseAnim.play_backwards("Pause")
+
+
+func _on_menu_pressed():
+	save_high_score()
+	get_tree().paused=false
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+
+
+func _on_quit_pressed():
+	save_high_score()
+	get_tree().quit()
